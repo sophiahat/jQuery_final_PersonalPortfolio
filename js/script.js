c1eff5963c874341307bd485af2c1cec8d473ca0
Chris Spencer <chris@chrisspencercreative.com>
2015-11-14 14:58:07 -0800

    validation form added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
+//validation function
 
-$('document').ready(function() {
+
+function validateForm(form) {
+    $('#guest-form').validate({
+        rules: {
+            firstname: {
+                required: true,
+                minlength: 2
+            },
+            lastname: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            website: {
+                url: true
+            
+            },
+            country: {
+                required: true,
+                minlength: 2
+            },
+
+            comments: {
+                minlength: 5
+            }
+        },
+        messages: {
+            firstname: {
+                required: "First Name is required",
+                minlength: "Your First Name must be at least 2 characters"
+            },
+            lastname: {
+                required: "Last Name is required",
+                minlength: "Your Last Name must be at least 2 characters"
+            },
+            email: {
+                required: "Email is required, everyone has an email, right?",
+                email: "Please enter a valid email"
+            },
+            website: {
+                url: "Please enter a valid url beginning with 'http://'"
+            
+            },
+            country: {
+                required: "Country is required",
+                minlength: "Country must be at least 2 characters"
+            },
+
+            comments: {
+                minlength: "Comments must be at least 5 characters"
+            }        
+        }
+    });
+                             
+}
+
+$(document).ready(function() {
     var path = location.pathname;
     var pathArray = path.split('/');
     var pArrLength = pathArray.length;
